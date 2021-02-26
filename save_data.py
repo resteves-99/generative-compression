8728979ae5792cce68a7f1d3d786054ca98581a9
resteves-99 <rafaele675@gmail.com>
2021-02-25 17:47:10 -0800

    fixed minor issues:

--- save_data.py
+++ save_data.py
@@ -7,7 +7,7 @@ train_data, test_data, validation_data = tfds.load(
                                                     'celeb_a',
                                                     split=['train', 'test', 'validation'],
                                                     data_dir='./data',
-                                                    download=True,
+                                                    download=False,
     
                                                 )
 
@@ -18,12 +18,13 @@ for ex in train_data:
     image_array = image_array.numpy()
     image = Image.fromarray(image_array)
 
-    path = './data/celeb_a_png/train/' + str(i)
+    path = './data/celeb_a_png/train/pic_' + str(i)
 
     image.save(path, format='PNG')
     train_paths.append(path)
 
     if i%10000==0: print('train conversion ', i, '/162770')
+    i+=1
 
 test_paths = []
 i = 0
@@ -32,12 +33,13 @@ for ex in test_data:
     image_array = image_array.numpy()
     image = Image.fromarray(image_array)
 
-    path = './data/celeb_a_png/test/' + str(i)
+    path = './data/celeb_a_png/test/pic_' + str(i)
 
     image.save(path, format='PNG')
     test_paths.append(path)
     
     if i%5000==0: print('train conversion ', i, '/19962')
+    i+=1
 
 
 validation_paths = []
@@ -47,19 +49,20 @@ for ex in validation_data:
     image_array = image_array.numpy()
     image = Image.fromarray(image_array)
 
-    path = './data/celeb_a_png/train/' + str(i)
+    path = './data/celeb_a_png/train/pic_' + str(i)
 
     image.save(path, format='PNG')
     validation_paths.append(path)
     
     if i%5000==0: print('train conversion ', i, '/19867')
+    i+=1
 
-train_df = pd.DataFrame(data=train_paths, columns='path', key='df')
-train_hdf = train_df.to_hdf('./data/celeb_paths/train_paths.h5')
+train_df = pd.DataFrame(data=train_paths, columns='path')
+train_hdf = train_df.to_hdf('./data/celeb_paths/train_paths.h5', key='df')
 
-test_df = pd.DataFrame(data=test_paths, columns='path', key='df')
-test_hdf = test_df.to_hdf('./data/celeb_paths/test_paths.h5')
+test_df = pd.DataFrame(data=test_paths, columns='path')
+test_hdf = test_df.to_hdf('./data/celeb_paths/test_paths.h5', key='df')
 
-validation_df = pd.DataFrame(data=validation_paths, columns='path', key='df')
-validation_hdf = validation_df.to_hdf('./data/celeb_paths/validation_paths.h5')
+validation_df = pd.DataFrame(data=validation_paths, columns='path')
+validation_hdf = validation_df.to_hdf('./data/celeb_paths/validation_paths.h5', key='df')
 
